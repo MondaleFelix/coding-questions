1057302c60b2963ab34df29cdb652748e5b2e3fc
MondaleFelix <mondalefelix@yahoo.com>
2020-08-10 10:51:31 -0700

    completed pair_sum

--- pair_sum.py
+++ pair_sum.py
@@ -1,16 +1,27 @@
-#  Given an integer array, output all the unique pairs that sum
- # up to a specific value k
+# Given an integer array, output all the unique pairs that sum up to a specific value k
 
+# Input: [Int], Int
+# Output: [[Int]] || []
 
+# Test Case: [1,3,2,2], 4
+# Output: [[1,3], [2,2]]
 
-def pair_sum(num_list, k):
+# Create an empty array to store pairs
+# Create a hashtable to store elements of the array
+# Iterate through the given array
+	# Check if k - current element is in array
+		# Add to the pair array
+# Return pair array
 
-	for num1 in num_list:
-		for  num2 in num_list:
-			if num1 + num2 == k:
-				print((num1,num2))
+def pair_sum(arr, k):
+	pairs = []
+	elements = {}
 
+	for number in arr:
+		if k - number in elements:
+			pairs.append((number, k - number))
+		elements[number] = True
 
+	return pairs
 
-
-pair_sum([1,4,5,2,4], 10)
+print(pair_sum([1,3,2,2], 4))
